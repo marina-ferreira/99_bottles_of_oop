afc4b9befd3aedc3ddec2d7959af6f5d35c93cee
Marina Ferreira <marina.ferreira.developer@gmail.com>
2021-01-08 07:33:35 -0300

    Chapeter 6 | Replacing Conditionals with Polymorphism Everywhere

---
+++ chapter_6/04_replacing_conditionals_with_polymorphism_everywhere.rb
@@ -0,0 +1,87 @@
+class Bottles
+  def song
+    verses(99,0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).collect {|i| verse(i)}.join("\n")
+  end
+
+  def bottle_number_for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
+  end
+
+  def verse(number)
+    bottle_number = bottle_number_for(number)
+    next_bottle_number = bottle_number_for(bottle_number.successor)
+
+    "#{bottle_number} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{next_bottle_number} of beer on the wall.\n"
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def quantity
+    number.to_s
+  end
+
+  def container
+    "bottles"
+  end
+
+  def action
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def pronoun
+    "one"
+  end
+
+  def successor
+    number - 1
+  end
+
+  def to_s
+    "#{quantity} #{container}"
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    99
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
+end
+
