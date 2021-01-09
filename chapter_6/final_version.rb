d93b41dbb2d6b2110e8b2b728fca9ba9b44d85a9
Marina Ferreira <marina.ferreira.developer@gmail.com>
2021-01-09 10:03:23 -0300

    Chapter 6 | Final Version

---
+++ chapter_6/final_version.rb
@@ -0,0 +1,97 @@
+class Bottles
+  def song
+    verses(99,0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).collect {|i| verse(i)}.join("\n")
+  end
+
+  def verse(number)
+    bottle_number = BottleNumber.for(number)
+
+    "#{bottle_number} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{bottle_number.successor} of beer on the wall.\n"
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def quantity
+    number.to_s
+  end
+
+  def container
+    "bottles"
+  end
+
+  def action
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def pronoun
+    "one"
+  end
+
+  def successor
+    BottleNumber.for(number - 1)
+  end
+
+  def to_s
+    "#{quantity} #{container}"
+  end
+
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    when 6
+      BottleNumber6
+    else
+      BottleNumber
+    end.new(number)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    BottleNumber.for(99)
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def quantity
+    1
+  end
+
+  def container
+    "six-pack"
+  end
+end
